9cdf70175199b4eafc964db635a0d4fac03ead23
ZohoCorp <raghava.d@zohocorp.com>
2024-08-06 13:24:53 +0530

    Center alignement issue fixed.

---
+++ Sources/SwiftUIGenericListView/Views/CenteringScrollView.swift
@@ -0,0 +1,52 @@
+//
+//  CenteringScrollView.swift
+//  
+//
+//  Created by Raghava Dokala on 06/08/24.
+//
+import SwiftUI
+
+public struct CenteringScrollView<Content: View>: View {
+    let content: Content
+    let shouldCenter: Bool
+    
+    @State private var contentHeight: CGFloat = .zero
+    @State private var contentWidth: CGFloat = .zero
+    
+    public init(shouldCenter: Bool, @ViewBuilder content: () -> Content) {
+        self.content = content()
+        self.shouldCenter = shouldCenter
+    }
+    
+    public var body: some View {
+        VStack {
+            GeometryReader { geometry in
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        if shouldCenter {
+                            Spacer(minLength: max(0, (geometry.size.width - contentWidth) / 2))
+                        }
+                        content
+                            .background(GeometryReader { contentGeometry in
+                                Color.clear
+                                    .preference(key: ContentHeightKey.self, value: contentGeometry.size.height)
+                                    .onAppear {
+                                        self.contentWidth = contentGeometry.size.width
+                                    }
+                                    .onChange(of: contentGeometry.size.width) { oldValue, newValue in
+                                        self.contentWidth = newValue
+                                    }
+                            })
+                        if shouldCenter {
+                            Spacer(minLength: max(0, (geometry.size.width - contentWidth) / 2))
+                        }
+                    }
+                }
+                .onPreferenceChange(ContentHeightKey.self) { value in
+                    self.contentHeight = value
+                }
+            }
+            .frame(height: contentHeight)
+        }
+    }
+}
--- Sources/SwiftUIGenericListView/Views/HorizontalSectionView.swift
+++ Sources/SwiftUIGenericListView/Views/HorizontalSectionView.swift
@@ -7,41 +7,21 @@
 
 import SwiftUI
 
-struct HorizontalSectionView: View {
+public struct HorizontalSectionView: View {
     let items: [AnyView]
     let centerAlign: Bool
+    var contentHeight: CGFloat = 0
          
-    @State private var contentHeight: CGFloat = 0
-         
-    var body: some View {
-        GeometryReader { geometry in
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 10) {
-                    if centerAlign {
-                        Spacer(minLength: max((geometry.size.width - totalWidth(in: geometry)) / 2, 0))
-                    }
+    public var body: some View {
+        VStack {
+            CenteringScrollView(shouldCenter: centerAlign) {
+                HStack(spacing: 20) {
                     ForEach(0..<items.count, id: \.self) { index in
                         items[index]
-                            .background(HeightReader())
-                            .onPreferenceChange(ContentHeightKey.self) { height in
-                                contentHeight = max(contentHeight, height)
-                            }
-                    }
-                    if centerAlign {
-                        Spacer(minLength: max((geometry.size.width - totalWidth(in: geometry)) / 2, 0))
                     }
                 }
-                .frame(maxWidth: .infinity)
             }
         }
-        .frame(height: contentHeight)
-    }
-         
-    private func totalWidth(in geometry: GeometryProxy) -> CGFloat {
-        let totalWidth = items.reduce(0) { sum, item in
-            let itemSize = UIHostingController(rootView: item).view.intrinsicContentSize
-            return sum + itemSize.width
-        }
-        return totalWidth
     }
 }
+
